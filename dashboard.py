7d24e4aa9db64ff8c8254b7a0a263f5e89db2e97
mtalimanchuk <mtalimanchuk@gmail.com>
2019-12-05 20:00:05 +0300

    add sqlite persistence, scheduled dump

--- dashboard.py
+++ dashboard.py
@@ -5,22 +5,6 @@ from flask import Flask, render_template, redirect, url_for
 from flask_apscheduler import APScheduler
 
 
-SQL_INSERT = {
-    "sms": """
-    INSERT INTO sms (type, date, address, status, message)
-    SELECT :Type, :Date, :Address, :Status, :Message
-    WHERE NOT EXISTS(SELECT 1 FROM sms WHERE type =:Type AND date =:Date AND address =:Address AND status =:Status AND message =:Message );""",
-    "calls": """
-    INSERT INTO calls (type, date, number, name, duration)
-    SELECT :Type, :Date, :Number, :Name, :Duration
-    WHERE NOT EXISTS(SELECT 1 FROM calls WHERE type =:Type AND date =:Date AND number =:Number AND name =:Name AND duration =:Duration );""",
-    "contacts": """
-    INSERT INTO contacts (number, name)
-    SELECT :Number, :Name
-    WHERE NOT EXISTS(SELECT 1 FROM contacts WHERE number =:Number AND name =:Name );""",
-}
-
-
 class Config(object):
     SCHEDULER_API_ENABLED = True
     DB = "dump.db"
@@ -31,19 +15,13 @@ app = Flask(__name__, template_folder="")
 app.config.from_object(Config())
 
 
-@scheduler.task('interval', id='dump_update', seconds=10, misfire_grace_time=900)
-def dump_update():
-    db = app.config['DB']
+def dump_parse_save_unique(requested_dumps):
+    # insert meterpreter dump here
 
-    with sqlite3.connect(db) as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS sms (type TEXT, date TEXT, address TEXT, status TEXT, message TEXT);")
-        conn.execute("CREATE TABLE IF NOT EXISTS calls (type TEXT, date TEXT, number TEXT, name TEXT, duration TEXT);")
-        conn.execute("CREATE TABLE IF NOT EXISTS contacts (number TEXT, name TEXT);")
-
-    requested_dumps = ["sms", "calls", "contacts"]
+    # after the dumps are done, they will be picked up
     for dump_type in requested_dumps:
         meta = {'DumpType': dump_type}
-        headers = []
+        # headers = []
         dump = {}
         try:
             with open(f"{dump_type}.txt", 'r', encoding='utf-8') as dump_f:
@@ -51,36 +29,41 @@ def dump_update():
                 while True:
                     line = next(dump_f).strip()
                     if line:
-                        if line[0] == "#":
+                        if line.startswith("#"):
                             idx = line
                             dump[idx] = {}
                         elif idx:
                             key, value = [s.strip() for s in line.split(":", maxsplit=1)]
                             dump[idx][key] = value
-                            if key not in headers:
-                                headers.append(key)
+                            # if key not in headers:
+                            #     headers.append(key)
                         elif ":" in line:
                             key, value = [s.strip() for s in line.split(":", maxsplit=1)]
                             meta[key] = value
         except StopIteration:
             dump = list(dump.values())
-
-        with sqlite3.connect(db) as conn:
-            for row in dump:
-                conn.execute(SQL_INSERT[dump_type], row)
-    print('Dump!')
+            with sqlite3.connect(app.config['DB']) as conn:
+                for row in dump:
+                    sql_insert(conn, dump_type, row)
 
 
-@app.route('/')
-def index():
-    return redirect(url_for('dashboard'))
+def sanitize(query: str):
+    return ''.join(c for c in query if c.isalnum())
 
 
-@app.route('/dashboard')
-def dashboard():
+def sql_insert(conn, table, row_dict):
+    table = sanitize(table)
+    headers = [sanitize(k).lower() for k in row_dict.keys()]
+    data_placeholders = [f":{sanitize(k)}" for k in row_dict.keys()]
+    conditions = " AND ".join(f"{header} ={data_placeholder}" for header, data_placeholder in zip(headers, data_placeholders))
+    marked_up_query = f"INSERT INTO {table} ({', '.join(headers)}) SELECT {', '.join(data_placeholders)} WHERE NOT EXISTS(SELECT 1 FROM {table} WHERE {conditions});"
+    conn.execute(marked_up_query, row_dict)
+
+
+def sql_load(requested_dumps):
     dumps = {}
     with sqlite3.connect(app.config['DB']) as conn:
-        for dump_type in ["sms", "calls", "contacts"]:
+        for dump_type in requested_dumps:
             table_name = ''.join(c for c in dump_type if c.isalpha())
             dumps[table_name] = {}
             cur = conn.cursor()
@@ -91,6 +74,27 @@ def dashboard():
 
             cur.execute(f"SELECT * FROM {table_name};")
             dumps[table_name]['rows'] = cur.fetchall()
+    return dumps
+
+@scheduler.task('interval', id='dump_update', seconds=10, misfire_grace_time=900)
+def dump_update():
+    with sqlite3.connect(app.config['DB']) as conn:
+        conn.execute("CREATE TABLE IF NOT EXISTS sms (type TEXT, date TEXT, address TEXT, status TEXT, message TEXT);")
+        conn.execute("CREATE TABLE IF NOT EXISTS calls (type TEXT, date TEXT, number TEXT, name TEXT, duration TEXT);")
+        conn.execute("CREATE TABLE IF NOT EXISTS contacts (number TEXT, name TEXT);")
+
+    dump_parse_save_unique(["sms", "calls", "contacts"])
+    print('Dump!')
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('dashboard'))
+
+
+@app.route('/dashboard')
+def dashboard():
+    dumps = sql_load(["sms", "calls", "contacts"])
     # print(dumps)
     return render_template("dashboard.html.j2", dumps=dumps)
 
