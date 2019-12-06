c63fc08457d12b4b466f8170efbc5a7be5880405
mtalimanchuk <mtalimanchuk@gmail.com>
2019-12-06 06:04:06 +0300

    add fetch update

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sqlite3
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, jsonify, render_template, redirect, request, url_for
 from flask_apscheduler import APScheduler
 
 
@@ -24,7 +24,7 @@ def dump_parse_save_unique(requested_dumps):
         # headers = []
         dump = {}
         try:
-            with open(f"{dump_type}.txt", 'r', encoding='utf-8') as dump_f:
+            with open(f"dump/{dump_type}.txt", 'r', encoding='utf-8') as dump_f:
                 idx = None
                 while True:
                     line = next(dump_f).strip()
@@ -45,6 +45,8 @@ def dump_parse_save_unique(requested_dumps):
             with sqlite3.connect(app.config['DB']) as conn:
                 for row in dump:
                     sql_insert(conn, dump_type, row)
+        except FileNotFoundError:
+            print("")
 
 
 def sanitize(query: str):
@@ -55,12 +57,12 @@ def sql_insert(conn, table, row_dict):
     table = sanitize(table)
     headers = [sanitize(k).lower() for k in row_dict.keys()]
     data_placeholders = [f":{sanitize(k)}" for k in row_dict.keys()]
-    conditions = " AND ".join(f"{header} ={data_placeholder}" for header, data_placeholder in zip(headers, data_placeholders))
+    conditions = " AND ".join(f"{header} = {data_placeholder}" for header, data_placeholder in zip(headers, data_placeholders))
     marked_up_query = f"INSERT INTO {table} ({', '.join(headers)}) SELECT {', '.join(data_placeholders)} WHERE NOT EXISTS(SELECT 1 FROM {table} WHERE {conditions});"
     conn.execute(marked_up_query, row_dict)
 
 
-def sql_load(requested_dumps):
+def sql_load(requested_dumps, except_ids=None):
     dumps = {}
     with sqlite3.connect(app.config['DB']) as conn:
         for dump_type in requested_dumps:
@@ -72,16 +74,40 @@ def sql_load(requested_dumps):
             table_info = cur.fetchall()
             dumps[table_name]['headers'] = [h[1] for h in table_info]
 
-            cur.execute(f"SELECT * FROM {table_name};")
+            condition = ""
+            if except_ids:
+                ids = ", ".join(sanitize(str(_id)) for _id in except_ids[dump_type])
+                condition = f"WHERE id NOT IN ({ids})"
+            cur.execute(f"SELECT * FROM {table_name} {condition} ORDER BY id DESC;")
             dumps[table_name]['rows'] = cur.fetchall()
     return dumps
 
-@scheduler.task('interval', id='dump_update', seconds=10, misfire_grace_time=900)
+
+@scheduler.task('interval', id='mock_meterpreter', seconds=1, misfire_grace_time=900)
+def mock_meterpreter():
+    import random
+    from datetime import datetime
+
+    if random.randint(1, 100) > 70:
+        mocks = {
+            "sms": (f"\n\n#{random.randint(100, 10000)}\n"
+                    f"Type  : {random.choice(['Incoming', 'Outgoing'])}\n"
+                    f"Date  : {datetime.now()}\n"
+                    f"Address  : {''.join(random.choice(list('0123456789')) for _ in range(random.randint(3, 11)))}\n"
+                    f"Status  : HUYATUS\n"
+                    f"Message  : {''.join(random.choice(list('abcdefghijklmnopqrstuvwxyz    ')) for _ in range(random.randint(10, 100)))}"
+                    )
+        }
+        with open(f"dump/sms.txt", "a", encoding='utf-8') as dump_f:
+            dump_f.write(mocks['sms'])
+
+
+@scheduler.task('interval', id='dump_update', seconds=3, misfire_grace_time=900)
 def dump_update():
     with sqlite3.connect(app.config['DB']) as conn:
-        conn.execute("CREATE TABLE IF NOT EXISTS sms (type TEXT, date TEXT, address TEXT, status TEXT, message TEXT);")
-        conn.execute("CREATE TABLE IF NOT EXISTS calls (type TEXT, date TEXT, number TEXT, name TEXT, duration TEXT);")
-        conn.execute("CREATE TABLE IF NOT EXISTS contacts (number TEXT, name TEXT);")
+        conn.execute("CREATE TABLE IF NOT EXISTS sms (id INTEGER PRIMARY KEY, type TEXT, date TEXT, address TEXT, status TEXT, message TEXT);")
+        conn.execute("CREATE TABLE IF NOT EXISTS calls (id INTEGER PRIMARY KEY, type TEXT, date TEXT, number TEXT, name TEXT, duration TEXT);")
+        conn.execute("CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY, number TEXT, name TEXT);")
 
     dump_parse_save_unique(["sms", "calls", "contacts"])
     print('Dump!')
@@ -95,8 +121,17 @@ def index():
 @app.route('/dashboard')
 def dashboard():
     dumps = sql_load(["sms", "calls", "contacts"])
-    # print(dumps)
+    print(dumps)
     return render_template("dashboard.html.j2", dumps=dumps)
+
+
+@app.route('/dashboard/update', methods=['POST'])
+def update():
+    known_ids = request.get_json(force=True)
+    # print(f"Client knows {known_ids}")
+    dumps = sql_load(["sms", "calls", "contacts"], except_ids=known_ids)
+    # print(f"Sending {dumps}")
+    return jsonify(dumps)
 
 
 # don't put these into "ifmain"
